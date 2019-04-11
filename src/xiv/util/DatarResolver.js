2109d1bf997b4846ac22c0380e8e4374b15b2eb9
almostSouji <timoqueezle@gmail.com>
2019-04-11 13:21:22 +0200

    xiv: make resolvers skip id fetching if isNaN

--- src/xiv/util/DatarResolver.js
+++ src/xiv/util/DatarResolver.js
@@ -13,40 +13,50 @@ class DataResolver {
 	 * @returns {Promise<Object>}
 	 */
 	async resolveCharacter(serverOrID, name) {
+		const handleSearch = async (server, charname) => {
+			if (!charname) {
+				return Promise.reject(new Error('missing name'));
+			}
+			if (!this.xiv.resources.servers.find(s => s.toLowerCase() === server.toLowerCase())) {
+				return Promise.reject(new Error('invalid server'));
+			}
+			try {
+				const { Results } = await this.xiv.character.search(charname, {
+					page: 1,
+					server
+				});
+				if (!Results.length) {
+					return Promise.resolve(null);
+				}
+				const { Character: char } = await this.xiv.character.get(Results[0].ID, {
+					extended: true
+				});
+				return Promise.resolve(char);
+			} catch (err) {
+				if (err.statusCode === 406) {
+					return Promise.reject(new Error('invalid arguments'));
+				}
+				return Promise.reject(err);
+			}
+		};
+
 		if (!serverOrID) {
 			return Promise.reject(new Error('no parameters'));
 		}
+		if (isNaN(serverOrID)) {
+			return handleSearch(serverOrID, name);
+		}
 		try {
+			if (isNaN(serverOrID)) {
+				throw new Error({ message: 'invalid id', statusCode: 406 });
+			}
 			const { Character: char } = await this.xiv.character.get(serverOrID, {
 				extended: true
 			});
 			return Promise.resolve(char);
 		} catch (e) {
 			if (e.statusCode === 406) {
-				if (!name) {
-					return Promise.reject(new Error('missing name'));
-				}
-				if (!this.xiv.resources.servers.find(s => s.toLowerCase() === serverOrID.toLowerCase())) {
-					return Promise.reject(new Error('invalid server'));
-				}
-				try {
-					const { Results } = await this.xiv.character.search(name, {
-						page: 1,
-						server: serverOrID
-					});
-					if (!Results.length) {
-						return Promise.resolve(null);
-					}
-					const { Character: char } = await this.xiv.character.get(Results[0].ID, {
-						extended: true
-					});
-					return Promise.resolve(char);
-				} catch (err) {
-					if (err.statusCode === 406) {
-						return Promise.reject(new Error('invalid arguments'));
-					}
-					return Promise.reject(err);
-				}
+				return handleSearch(serverOrID, name);
 			}
 			return Promise.resolve(null);
 		}
@@ -59,8 +69,38 @@ class DataResolver {
 	 * @returns {Promise<Object>}
 	 */
 	async resolveFreeCompany(serverOrID, name) {
+		const handleSearch = async (server, fcname) => {
+			if (!fcname) {
+				return Promise.reject(new Error('missing name'));
+			}
+			if (!this.xiv.resources.servers.find(s => s.toLowerCase() === server.toLowerCase())) {
+				return Promise.reject(new Error('invalid server'));
+			}
+			try {
+				const { Results } = await this.xiv.freecompany.search(fcname, {
+					page: 1,
+					server
+				});
+				console.log(Results);
+				if (!Results.length) {
+					return Promise.resolve(null);
+				}
+				const { FreeCompany: company } = await this.xiv.freecompany.get(Results[0].ID, {
+					extended: true
+				});
+				return Promise.resolve(company);
+			} catch (err) {
+				if (err.statusCode === 406) {
+					return Promise.reject(new Error('invalid arguments'));
+				}
+				return Promise.reject(err);
+			}
+		};
 		if (!serverOrID) {
 			return Promise.reject(new Error('no parameters'));
+		}
+		if (isNaN(serverOrID)) {
+			return handleSearch(serverOrID, name);
 		}
 		try {
 			const { FreeCompany: company } = await this.xiv.freecompany.get(serverOrID, {
@@ -69,31 +109,7 @@ class DataResolver {
 			return Promise.resolve(company);
 		} catch (e) {
 			if (e.statusCode === 406) {
-				if (!name) {
-					return Promise.reject(new Error('missing name'));
-				}
-				if (!this.xiv.resources.servers.find(s => s.toLowerCase() === serverOrID.toLowerCase())) {
-					return Promise.reject(new Error('invalid server'));
-				}
-				try {
-					const { Results } = await this.xiv.freecompany.search(name, {
-						page: 1,
-						server: serverOrID
-					});
-					console.log(Results);
-					if (!Results.length) {
-						return Promise.resolve(null);
-					}
-					const { FreeCompany: company } = await this.xiv.freecompany.get(Results[0].ID, {
-						extended: true
-					});
-					return Promise.resolve(company);
-				} catch (err) {
-					if (err.statusCode === 406) {
-						return Promise.reject(new Error('invalid arguments'));
-					}
-					return Promise.reject(err);
-				}
+				return handleSearch(serverOrID, name);
 			}
 			return Promise.resolve(null);
 		}
