c98040a3516ce3512de10fc01481a417c96c753d
almostSouji <timoqueezle@gmail.com>
2019-05-27 08:42:07 +0200

    cai: clean up nuke cmd

--- src/cai/commands/administration/nuke.js
+++ src/cai/commands/administration/nuke.js
@@ -5,37 +5,37 @@ const { format } = require('date-fns');
 class NukeCommand extends Command {
 	constructor() {
 		super('nuke', {
-			aliases: ['nuke', 'obliterate'],
+			aliases: ['nuke', 'cybernuke', 'antiraidnuke'],
 			description: {
 				content: '[PROTECTED FILE]... time in minutes',
-				usage: '<server age to survive> <account age to survive>'
+				usage: '<min server age to survive> <min account age to survive>'
 			},
 			clientPermissions: ['BAN_MEMBERS'],
 			userPermissions: ['ADMINISTRATOR'],
 			channel: 'guild',
 			args: [
 				{
-					id: 'joined',
+					id: 'joinedBefore',
 					type: Argument.range('number', 0.1, Infinity, true)
 				},
 				{
-					id: 'accountage',
+					id: 'olderThan',
 					type: Argument.range('number', 0.1, Infinity, true)
 				}
 			]
 		});
 	}
 
-	async exec(msg, { joined, accountage }) {
+	async exec(msg, { joinedBefore, olderThan }) {
 		await msg.util.send('Calculating nuke targets...');
 		await msg.guild.members.fetch();
-		const joinCutoff = Date.now() - (joined * 60000);
-		const ageCutoff = Date.now() - (accountage * 60000);
+		const joinCutoff = Date.now() - (joinedBefore * 60000);
+		const ageCutoff = Date.now() - (olderThan * 60000);
 		const targets = msg.guild.members.filter(
 			member => member.joinedTimestamp > joinCutoff &&
 				member.user.createdTimestamp > ageCutoff
 		);
-		await msg.util.send(`Damage calculation: ${targets.size} members will be hit, proceed?`);
+		await msg.util.send(`Damage calculation: ${targets.size} members will be hit, proceed? [y]es`);
 		const responses = await msg.channel.awaitMessages(m => m.author.id === msg.author.id,
 			{
 				max: 1,
@@ -50,7 +50,7 @@ class NukeCommand extends Command {
 			return msg.reply('Nuke cancelled.');
 		}
 
-		await msg.reply(`Make sure you __really__ want to do this! This action is irreversible and is targeting ${targets.size} member(s)!`);
+		await msg.reply(`Make sure you __really__ want to do this! This action is irreversible and is targeting ${targets.size} member(s) that are about to be banned! [y]es`);
 
 		const finalResponses = await msg.channel.awaitMessages(m => m.author.id === msg.author.id,
 			{
@@ -63,68 +63,34 @@ class NukeCommand extends Command {
 			return msg.reply('Nuke cancelled.');
 		}
 
-		const statusMessage = await finalResponse.reply('Commencing cybernuke...');
+		const statusMessage = await finalResponse.reply('Commencing nuke...');
 
 		const fatalities = [];
 		const survivors = [];
-		const promises = [];
 
 		let counter = 0;
-
-		let idx = 0;
-
-
-		console.log('before loop');
 		for (const member of targets.values()) {
-			// promises.push(new Promise(async resolve => {
-			const id = idx++;
-			console.log(`${id} start`);
-			console.log(`${id} ${member.user.tag}`);
-			// https://github.com/discordjs/discord.js/blob/master/src/structures/interfaces/TextBasedChannel.js#L127
-
-			// ??ev msg.guild.member("445864099172319242").send("hi")
-			// setTimeout(async () => {
-			try {
-				const resp = await member.send(stripIndents`
+			member.send(stripIndents`
 					You have been hit by a nuke in \`${msg.guild.name}\`.
 					This means you have been banned from the server, likely during a server raid.
 					If you think this is a mistake please contact the staff of this server.
 
-					The nuke has been commenced by ${msg.member} | \`${msg.member.displayName}\` | \`${msg.author.tag}\` (\`${msg.member.id}\`) on ${format(Date.now(), "MMM do yyyy 'at' kk:mm:ss")}`);
-				console.log(`${id} after send ${resp}`);
-			} catch (e) {
-				console.log(`${id} send error${e}`);
-			}
+					The nuke has been commenced by ${msg.member} | \`${msg.member.displayName}\` | \`${msg.author.tag}\` (\`${msg.member.id}\`) on ${format(Date.now(), "MMM do yyyy 'at' kk:mm:ss")}`).catch(() => {});
 
 			try {
-				await member.ban({ reason: `Cybernuke commenced by ${msg.author.tag} (${msg.author.id})` });
-				console.log(`${id} after ban`);
+				await member.ban({ reason: `Nuke commenced by ${msg.author.tag} (${msg.author.id})` });
 				fatalities.push(member);
 			} catch (e) {
-				console.log(`${id} ban error ${e}`);
 				survivors.push({
 					member,
 					error: e
 				});
 			}
-
-			console.log(`${id} counter ${counter}`);
 			counter++;
-			console.log(`${id} counter ${counter}`);
 			if (counter % 5 === 0) {
-				await statusMessage.edit(`Launching cyber nuke (${Math.round(counter / targets.size * 100)}%)...`);
+				await statusMessage.edit(`Launching nuke (${Math.round(counter / targets.size * 100)}%)...`);
 			}
-			// 		resolve();
-			// 	}, 0 * id);
-			// }));
 		}
-		console.log('after loop');
-		// const interval = setInterval(() => {
-		// 	console.log('p', promises);
-		// }, 2000);
-
-		// await Promise.all(promises);
-		// clearInterval(interval);
 		await statusMessage.edit('Impact confirmed. Report incoming...');
 
 		const report = [];
@@ -138,10 +104,7 @@ class NukeCommand extends Command {
 			report.push(`__**Survivors [${survivors.length}]:**__`);
 			report.push(survivors.map(srv => `**-** ${srv.member.displayName} (${srv.member.id}): \`${srv.error}\``).join('\n'));
 		}
-		return msg.channel.send(report.join('\n'));
+		return msg.channel.send(report.join('\n'), { split: true });
 	}
 }
 module.exports = NukeCommand;
-
-process.on('uncaughtException', e => { console.log('e', e); });
-process.on('unhandledRejection', r => { console.log('r', r); });
